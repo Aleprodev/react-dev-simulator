dfaaf28a4d7188a7bf5dfa51d9e2b9a8acfd3ed7
Alejandro Piña <jose@h2d.dev>
2024-12-08 23:18:14 -0300

    Cambio en el h1 de el archivo App.tsx

--- dia1/my-fist-project/src/App.tsx
+++ dia1/my-fist-project/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Algún cambio</h1>
       <div className="card">
         <button onClick={() => setCount(count + 1)}>
           count is {count}
