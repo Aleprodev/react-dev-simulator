9776c0da9f64d6452639a5b7d30169e2bfd85a17
Alejandro Piña <jose@h2d.dev>
2024-12-15 18:16:54 -0300

    Tailwind y componentes con props

--- dia1/my-fist-project/src/App.tsx
+++ dia1/my-fist-project/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CustomButton from './components/CustomButton'
 
 function App() {
   const [count, setCount] = useState<number>(0)
@@ -18,32 +16,21 @@ function App() {
   ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Algún cambio</h1>
-      <div className="card">
-        <button onClick={() => setCount(count + 1)}>
+    <div className='p-10'>
+      <h1 className='font-bold underline text-red-500 w-fit text-5xl'>Hola mundo</h1>
+        {/* Este es un botón con estilos condicionales */}
+        <button className={`${count > 10 ? 'bg-red-500' : 'bg-purple-700'} text-white px-4 py-2 font-bold rounded-full mt-5 hover:scale-110 transition-all`} onClick={() => setCount(count + 1)} >
           count is {count}
         </button>
 
-        {/* Esta es la impresión de un objeto */}
-        <p>{persona.nombre}: {persona.edad}</p>
+        {/* Esto es una condición if-else */}
+        {count > 10
+          ? <p>{persona.nombre}</p>
+          : personas.map((fulano, index) => <p key={index}>{fulano.nombre}</p>)
+        }
 
-        {/* Esta es la impresión de un array  */}
-        {personas.map((fulano, index) => <p key={index}>Hola, {fulano.nombre}, tienes {fulano.edad}</p>)}
-
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <CustomButton text={`Resta el contador ${count}`} bgColor='blue-800' cb={() => setCount(count - 1)} />
+    </div>
   )
 }
 
